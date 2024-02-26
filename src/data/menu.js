4d124dbb069ebb9d1efdf0fe9cb7dae6e7fa7caf
Frederick Roegiers <frederick@rogerthat.be>
2024-02-26 09:26:39 +0100

    wip

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,19 +1,22 @@
-export default [{
-        label: "Home",
-        target: "_self",
-        url: "/",
-    }, {
-        label: "About",
-        target: "_self",
-        url: "/about-us",
-    }, {
-        label: "Contact",
-        target: "_self",
-        url: "/contact",
-    },
-    {
-        label: "Visit us on Facebook",
-        target: "_blank",
-        url: "https://facebook.com",
-    },
+export default [
+  {
+    label: "Home",
+    target: "_self",
+    url: "/",
+  },
+  {
+    label: "About",
+    target: "_self",
+    url: "/about-us",
+  },
+  {
+    label: "Contact",
+    target: "_self",
+    url: "/contact",
+  },
+  {
+    label: "Visit us on Facebook",
+    target: "_blank",
+    url: "https://facebook.com",
+  },
 ];
